51c8667d139e629daa3fa7fb7d1d4faa6300be8d
heyaohua <yaohua_he@163.com>
2017-11-16 19:51:53 +0800

    增加测试

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 var http = require("http");  
 http.createServer(function(request, response) {    
     response.writeHead(200, {"Content-Type": "text/plain"});    
-    response.write("Hello World！！！！！");    
+    response.write("Hello World, 测试");    
     response.end();  
 }).listen(8888);  
 console.log("nodejs start listen 8888 port!");  
